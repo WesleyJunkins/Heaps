a70e49ad821762f9860075acb7ddf25ea28506a5
WesleyJunkins <67446010+WesleyJunkins@users.noreply.github.com>
2023-04-06 01:00:03 -0500

    Added functions

--- Heap.cpp
+++ Heap.cpp
@@ -1,3 +1,6 @@
+#ifndef heap
+#define heap
+
 #include <iostream>
 #include "CircularDynamicArray.cpp"
 using namespace std;
@@ -5,51 +8,157 @@ using namespace std;
 template <typename keytype>
 class Heap
 {
-    Public:
+public:
+    CircularDynamicArray<keytype> mainArray;
 
-    //Default Constructor
+    // Default Constructor
     Heap()
     {
 
     };
 
-    //Special Constructor
+    // Special Constructor
     Heap(keytype k[], int s)
     {
-
+        for (int i = 0; i < s; i++)
+        {
+            mainArray.addEnd(k[i]);
+        }
+        // int currentIndex = mainArray.eIndexActual();
+        for (int i = ((mainArray.length() - 2) / 2); i >= 0; i--)
+        {
+            heapify(i);
+        }
     };
 
-    //Destructor
-    ~Heap()
+    // Copy Constructor
+    Heap(const Heap& other)
     {
-
+        mainArray = CircularDynamicArray<keytype>(other.mainArray);
     };
 
-    //Copy Constructor
+    // Copy Assignment Operator
+    Heap& operator= (const Heap& other)
+    {
+        if(this != &other)
+        {
+            mainArray = CircularDynamicArray<keytype>(other.mainArray);
+        }
+        return *this;
+    }
 
-    //Copy Assignment Operator
-
-    //Show minimum. Make no changes.
+    // Show minimum. Make no changes.
     keytype peekKey()
     {
-
+        return mainArray[0];
     };
 
-    //Show minimum. Then remove it.
+    // Show minimum. Then remove it.
     keytype extractMin()
     {
-
+        keytype minKey = mainArray[0];
+        mainArray[0] = mainArray[mainArray.length() - 1];
+        mainArray.delEnd();
+        heapify(0);
+        return minKey;
     };
 
-    //Insert
+    // Insert
     void insert(keytype k)
     {
-
+        mainArray.addEnd(k);
+        int currentIndex = mainArray.eIndexActual();
+        bubbleUp(currentIndex);
     };
 
-    //Print all keys in the array, starting at the root.
+    // Print all keys in the array, starting at the root.
     void printKey()
     {
+        for (int i = 0; i < mainArray.length(); i++)
+        {
+            cout << mainArray[i] << " ";
+        }
+        cout << endl;
+    };
 
+    // Helper Functions
+    // index's parent
+    int parentOf(int index)
+    {
+        return (index - 1) / 2;
+    };
+
+    // index's left child
+    int leftChildOf(int index)
+    {
+        return (2 * index) + 1;
+    };
+
+    // index's right child
+    int rightChildOf(int index)
+    {
+        return (2 * index) + 2;
+    };
+
+    // Bubble Up Function
+    void bubbleUp(int index)
+    {
+        while ((index > 0) && (mainArray[index] < mainArray[parentOf(index)]))
+        {
+            keytype tempNode = mainArray[index];
+            mainArray[index] = mainArray[parentOf(index)];
+            mainArray[parentOf(index)] = tempNode;
+            index = parentOf(index);
+        }
+    };
+
+    // Heapify Function
+    void heapify(int index)
+    {
+        int smallestOfTheBunch = index;
+        int leftChild = leftChildOf(index);
+        int rightChild = rightChildOf(index);
+        if ((leftChild < mainArray.length()) && (mainArray[leftChild] < mainArray[smallestOfTheBunch]))
+        {
+            smallestOfTheBunch = leftChild;
+        }
+        if ((rightChild < mainArray.length()) && (mainArray[rightChild] < mainArray[smallestOfTheBunch]))
+        {
+            smallestOfTheBunch = rightChild;
+        }
+        if (smallestOfTheBunch != index)
+        {
+            keytype tempNode = mainArray[index];
+            mainArray[index] = mainArray[smallestOfTheBunch];
+            mainArray[smallestOfTheBunch] = tempNode;
+            heapify(smallestOfTheBunch);
+        }
     };
 };
+
+int main()
+{
+    Heap<int> T1;
+    T1.insert(3);
+    // T1.insert(4);
+    // T1.insert(5);
+    // T1.insert(6);
+    // T1.insert(7);
+    // T1.insert(8);
+    // T1.insert(9);
+    // T1.insert(10);
+    // T1.insert(11);
+    T1.insert(2);
+    T1.printKey();
+    cout<<T1.peekKey()<<endl;
+    T1.printKey();
+    cout<<T1.extractMin()<<endl;
+    T1.printKey();
+    cout<<T1.extractMin()<<endl;
+    T1.printKey();
+    cout<<T1.extractMin()<<endl;
+    T1.printKey();
+    return 0;
+}
+
+#endif
