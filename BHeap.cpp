ed9c77220950b696cdd537d511c3ed2df1f56e72
WesleyJunkins <Wjunkins13@yahoo.com>
2023-04-11 15:48:50 -0500

    More working code. Also fixed the order some functions performed

--- BHeap.cpp
+++ BHeap.cpp
@@ -158,7 +158,44 @@ public:
     {
         // Start at the beginning of H2's rootList
         // While H2's rootList is not empty, copy the node into H1's rootList
-        while (H2.rootList.length() != 0)
+        while (H2.rootList.length() > 0)
+        {
+            // Node *nodeToInsert = new Node(H2.rootList[0]->key);
+            Node *nodeToInsert = H2.rootList[0];
+            // buildHeapCopy(nodeToInsert, H2.rootList[0]);
+            //  Traverse H1 with the node to insert until you reach a node with greater or equal order. Leave node to insert before these.
+            rootList.addFront(nodeToInsert);
+            for (int i = 0; (i + 1) < rootList.length();)
+            {
+                if (rootList[i]->order() > rootList[i + 1]->order())
+                {
+                    Node *tempElement = rootList[i];
+                    rootList[i] = rootList[i + 1];
+                    rootList[i + 1] = tempElement;
+                }
+                else
+                {
+                    break;
+                }
+                extractMinFixup(0); //                                          THIS MAY NEED TO BE REMOVED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                i++;
+            }
+            // Destroy the element we just copied from H2
+            // delete H2.rootList[0];
+            H2.rootList.delFront();
+            extractMinFixup(0);
+            // Fixup the heap
+            // extractMinFixup(0);
+            findMin();
+        }
+    };
+
+    // Merge H2 into current heap. H2 will become empty.
+    void mergeSpecial(BHeap<keytype> &H2)
+    {
+        // Start at the beginning of H2's rootList
+        // While H2's rootList is not empty, copy the node into H1's rootList
+        while (H2.rootList.length() > 0)
         {
             Node *nodeToInsert = new Node(H2.rootList[0]->key);
             buildHeapCopy(nodeToInsert, H2.rootList[0]);
@@ -353,5 +390,91 @@ public:
         }
     };
 };
+
+int main()
+{
+    int T1KeyValues[20] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+    int T1MoreKeyValues[10] = {1, 2, 3, 4, 5, 11, 12, 13, 14, 15};
+    int T2MoreKeyValues[11] = {6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21};
+    string T1StringValues[10] = {"the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "log", "tonight"};
+
+    BHeap<int> T1;
+
+    //BHeap<string> T1(T1StringValues, 10);
+
+    for (int i = 0; i < 1000; i++)
+    {
+        T1.insert(i);
+    };
+
+    // cout<<"Printing T1 Before the stuff"<<endl;
+    // T1.printKey();
+
+    // BHeap<int> T1;
+    // BHeap<int> T2(T2MoreKeyValues, 11);
+    // BHeap<int> T2(T1);
+    BHeap<int> T2;
+    T2 = T1;
+
+    cout << "Printing T1" << endl;
+    T1.printKey();
+
+    cout << "Printing T2" << endl;
+    T2.printKey();
+
+    // cout << "Done" << endl;
+    // T1.printKey();
+
+    // cout << "Done" << endl;
+    // T2.printKey();
+
+    // T1.merge(T2);
+
+    // cout << "DONE" << endl;
+    // T1.printKey();
+
+    // cout << "DONE" << endl;
+    // T2.printKey();
+
+    // cout<<"I'm Here!"<<endl;
+    // T1.insert(3);
+    // cout<<"I made it!"<<endl;
+    // T1.insert(4);
+    // cout<<"I made it again!"<<endl;
+    // T1.insert(2);
+    // cout<<"I made it a third time!"<<endl;
+    // T1.printKey();
+    // cout<<endl;
+    // cout<<T1.peekKey()<<endl;
+
+    // for(int i = 0; i < 10000; i++)
+    // {
+    //     T1.insert(i);
+    //     // T1.printKey();
+    //     // cout<<endl;
+    // }
+    for(int i = 0; i < 1000; i++)
+    {
+        cout<<T2.extractMin()<<endl;
+        // T1.printKey();
+        // cout<<endl;
+    }
+
+    // T1.insert(1);
+    // T1.insert(2);
+    // T1.insert(3);
+
+    // cout << T1.extractMin() << endl;
+    // cout << T1.extractMin() << endl;
+    // cout << T1.extractMin() << endl;
+
+    cout << "Done" << endl;
+    T2.printKey();
+
+    // cout << endl;
+    //  cout<<T1.peekKey()<<endl;
+    //  cout<<T1.peekKey()<<endl;
+    return 0;
+}
 
 #endif
